6bd8a50e1ee8e49825a710a168295da06b1b13bf
Aled Cuda <aledvirgil@gmail.com>
2023-02-08 20:42:22 -0800

    parse detail mode

--- RC-Circuits/parse_instek.py
+++ RC-Circuits/parse_instek.py
@@ -24,12 +24,29 @@ def parse_instek(file):
     """
     import pandas as pd
     import numpy as np
-    isint = lambda s: s.isnumeric() or (len(s) > 1 and s[1:].isnumeric())
+    from math import isnan
+
+    def is_num(s):
+        try:
+            float(s)
+            return True
+        except:
+            return False
+
     data = pd.read_csv(file, names=[0, 1, 2])
     pivot = 0
-    while not isint(data[0][pivot]):
+    while not is_num(data[0][pivot]):
         pivot += 1
     metadata = {data[0][i] : data[1][i] for i in range(0, pivot - 1)}
+    if "Mode" in metadata.keys() and metadata["Mode"] == "Detail":
+        times, data = zip(
+            *[
+                (float(t), float(d))
+                for t, d in zip(data[0][pivot:], data[1][pivot:])
+                if not (isnan(float(t)) or isnan(float(d)))
+            ]
+        )
+        return np.array(data), np.array(times), metadata
     data_raw = np.array([int(i) for i in data[0][pivot:]])
     times = np.arange(len(data_raw))*float(metadata['Sampling Period'])
     voltages = data_raw*float(metadata['Vertical Scale'])/25
